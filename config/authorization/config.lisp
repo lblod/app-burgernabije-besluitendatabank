1c5a5d7e5560080b52eb2e935a978299dbc4fb9f
Niels Vandekeybus <progster@gmail.com>
2025-02-19 14:28:13 +0100

    upgrade sparql-parser to latest version
    - bump default heap space and limit logging to improve memory usage

--- config/authorization/config.lisp
+++ config/authorization/config.lisp
@@ -2,13 +2,13 @@
 ;;; delta messenger
 (in-package :delta-messenger)
 
-(add-delta-logger)
+;;(add-delta-logger)
 (add-delta-messenger "http://delta-notifier/")
 
 ;;;;;;;;;;;;;;;;;
 ;;; configuration
 (in-package :client)
-(setf *log-sparql-query-roundtrip* t)
+(setf *log-sparql-query-roundtrip* nil)
 (setf *backend* "http://triplestore:8890/sparql")
 
 
