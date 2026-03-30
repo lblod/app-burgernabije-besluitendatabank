48cb999ac5f466eaa0ff17c0351d20129dc23169
Nordine Bittich <contact@bittich.be>
2026-03-30 14:42:04 +0200

    fix script

--- scripts/delete-resource/app.mjs
+++ scripts/delete-resource/app.mjs
@@ -1,10 +1,15 @@
 import { updateSudo } from "@lblod/mu-auth-sudo";
+const derived = (process.env.DERIVED_FROM_URL || '').trim();
+if(!derived) {
+	console.log("please provide an url to delete (the url the agenda point was derived from)");
+	process.exit(-1);
+}
 const q = `PREFIX prov: <http://www.w3.org/ns/prov#>
 DELETE WHERE {
 graph ?g {
-  ?s prov:wasDerivedFrom <${process.env.DERIVED_FROM_URL}>; ?p ?o.
-}
-}`;
+  ?s prov:wasDerivedFrom <${derived}>.
+  ?s ?p ?o.
+}}`;
 
 async function main() {
   await updateSudo(q, {}, {});
